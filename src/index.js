53400c2db6be924985fbb19c176783a4e39ed5a3
Mark Birbeck <mark.birbeck@shell.com>
2022-02-13 13:34:05 +0000

    feat: [#1] add unified (#2)
    * chore: add node_modules to ignore file
    
    * chore: set package to ESM module
    
    * feat: add unified
    
    commit c28489811da67b40a8bf915358868b32a7edd14c
    Author: Mark Birbeck <mark.birbeck@shell.com>
    Date:   Sat Feb 12 18:45:17 2022 +0000
    
        docs: add a README
    
    commit 17fb4fe976ececfa2a677111b86121b9389f0db7
    Author: Mark Birbeck <mark.birbeck@shell.com>
    Date:   Sat Feb 12 18:44:57 2022 +0000
    
        feat: add a simple runner
    
    commit e0c505cc39a944e2cdc106bff9094bdc1c522acb
    Author: Mark Birbeck <mark.birbeck@shell.com>
    Date:   Sat Feb 12 18:42:08 2022 +0000
    
        fixup! chore: add lockfile
    
    commit b24bd8ad65c46b3969e001563e07da3ab84e741b
    Author: Mark Birbeck <mark.birbeck@shell.com>
    Date:   Sat Feb 12 18:40:49 2022 +0000
    
        fixup! feat: add example markdown
    
    commit ebc2dd903bdbd1d537dc13e5a68450f96553278a
    Author: Mark Birbeck <mark.birbeck@shell.com>
    Date:   Sat Feb 12 18:31:39 2022 +0000
    
        feat: add unified
    
    commit a598b0f3e7bf7e87c2a5813a94feb93fbbed07da
    Author: Mark Birbeck <mark.birbeck@shell.com>
    Date:   Sat Feb 12 18:19:59 2022 +0000
    
        feat: add example markdown
    
    commit e7216c0b4a92817092c1c8716ae6701e5584ee58
    Author: Mark Birbeck <mark.birbeck@shell.com>
    Date:   Sat Feb 12 18:18:04 2022 +0000
    
        chore: add lockfile
    
    commit 88fab71a3141388b7642c5314646321cf09e4ae0
    Author: Mark Birbeck <mark.birbeck@shell.com>
    Date:   Sat Feb 12 18:17:49 2022 +0000
    
        feat: add remark-rehype
    
    * feat: add slug
    
    * feat: add TOC
    
    * feat: add document wrapper
    
    * feat: add formatting

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import {stream} from 'unified-stream'
+import {unified} from 'unified'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import remarkSlug from 'remark-slug'
+import remarkToc from 'remark-toc'
+import rehypeDocument from 'rehype-document'
+import rehypeFormat from 'rehype-format'
+import rehypeStringify from 'rehype-stringify'
+
+const processor = unified()
+  .use(remarkParse)
+  .use(remarkSlug)
+  .use(remarkToc)
+  .use(remarkRehype)
+  .use(rehypeDocument, {title: 'Contents'})
+  .use(rehypeFormat)
+  .use(rehypeStringify)
+
+process.stdin.pipe(stream(processor)).pipe(process.stdout)
